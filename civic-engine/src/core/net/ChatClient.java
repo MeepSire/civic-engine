d79d0fc802c81e5f4e6236f7a8ef8a3b226ed9c1
philipp@dr-jean-jacques.de <philipp@dr-jean-jacques.de@d35286f8-d542-8fef-d8f9-01f4298c6c5b>
2011-03-18 08:53:42 +0000

    Swing interface for multiclientchat added.

--- civic-engine/src/core/net/ChatClient.java
+++ civic-engine/src/core/net/ChatClient.java
@@ -3,33 +3,43 @@
 package core.net;
 
 import java.io.IOException;
-import utils.OOUtil;
+import java.net.UnknownHostException;
 
-public class ChatClient {
+public class ChatClient  {
 
     String text = "";
+    public static String nick;
+    public static TCPClient client;
 
-    public static void main(String[] args) throws IOException{
+    public static void connect(String ip, int port){
+        client = new TCPClient(ip, port);
+    }
 
-        TCPClient[] client = new TCPClient[]{
-            new TCPClient("127.0.0.1", 1337)
-        };
-
-        String nick = OOUtil.readString("Nick: ");
-
-        while(true){
-            client[0].sendMessage(nick + ": " + OOUtil.readString("Me : "));
-
-            TCPPackage pkg = client[0].getRecievedPackage();
+    public static String send(String str) throws UnknownHostException, IOException{
+        client.sendMessage(str);
+        TCPPackage pkg = client.getRecievedPackage();
             String[] msg = pkg.getMessages();
-
+            String str2="";
             for(int i = 0; i < msg.length; i++){
 
-                if(msg[i] != null && !msg[i].equals("") && !msg[i].startsWith(nick)){
-                    System.out.println(msg[i]);
+                if(msg[i] != null && !msg[i].equals("")){
+                     str2 += msg[i]+"\n";
                 }
 
             }
+            return str2;
+    }
+
+    public static String update() throws UnknownHostException, IOException{
+        return send("");
+    }
+
+    public static void main(String[] args) throws IOException{
+
+        new Connect().setVisible(true);
+
+        while(true){
+                        
         }
 
     }
