09d5f998f263882e93cf77db91fa019654bdf15a
bastian.hinterleitner@web.de <bastian.hinterleitner@web.de@d35286f8-d542-8fef-d8f9-01f4298c6c5b>
2011-03-25 20:17:34 +0000

    Renamed some utils classes to proper names and rewrote the whole EasyXml class because it didnt work for multiple items with the same name. (exhausting!!!)

--- civic-engine/src/utils/Part.java
+++ civic-engine/src/utils/Part.java
@@ -5,78 +5,84 @@
 
 package utils;
 
+import org.w3c.dom.NodeList;
+
 /**
  *
  * @author Basti
  */
 class Part {
 
-    public Part[] part= new Part[9999];
-    private int parts = 0;
-    public String name;
-    public String info="";
-    private int deep;
+        public Part[] part= new Part[9999];
+        private int parts = 0;
+        public String name;
+        public String info="";
+        private int deep;
 
-    Part(String name,int d){
-        deep=d;
-        this.name = name;
-    }
+        Part(String name,int d){
+            deep=d;
+            this.name = name;
+        }
 
-    Part(String name, String info, int d){
-        deep=d;
-        this.name = name;
-        this.info = info;
-    }
-    
-    public Part addPart(String name){
-        part[parts]=new Part(name,deep+1);
-        Part p = part[parts];
-        parts++;
-        return p;
-    }
+        Part(NodeList nList, String name){
+            this.name = name;
+        }
 
-    public Part addPart(String name, String info){
-        part[parts]=new Part(name,info,deep+1);
-        Part p = part[parts];
-        parts++;
-        return p;
-    }
+        Part(String name, String info, int d){
+            deep=d;
+            this.name = name;
+            this.info = info;
+        }
 
-    public boolean remPart(int i){
-        try{
-            for(int j=i;j<parts;j++){
-                part[j]=part[j+1];
+        public Part addPart(String name){
+            part[parts]=new Part(name,deep+1);
+            Part p = part[parts];
+            parts++;
+            return p;
+        }
+
+        public Part addPart(String name, String info){
+            part[parts]=new Part(name,info,deep+1);
+            Part p = part[parts];
+            parts++;
+            return p;
+        }
+
+        public boolean remPart(int i){
+            try{
+                for(int j=i;j<parts;j++){
+                    part[j]=part[j+1];
+                }
+                return true;
             }
-            return true;
-        }
-        catch(Exception e){
-            return false;
-        }
-    }
-
-    public String[] getParts(){
-        String[] str = new String[parts];
-        for(int i=0;i<str.length;i++){
-            str[i]=part[i].name;
-        }
-        return str;
-    }
-
-    public String save(){
-        String str ="";
-        for(int v=0;v<deep;v++){str+="    ";}
-        str +="<"+name+">";
-        if (parts==0){
-            str += info;
-        }
-        else{
-            str +="\n";
-            for(int i=0;i<parts;i++){
-                str+=part[i].save();
+            catch(Exception e){
+                return false;
             }
+        }
+
+        public String[] getParts(){
+            String[] str = new String[parts];
+            for(int i=0;i<str.length;i++){
+                str[i]=part[i].name;
+            }
+            return str;
+        }
+
+        public String save(){
+            String str ="";
             for(int v=0;v<deep;v++){str+="    ";}
+            str +="<"+name+">";
+            if (parts==0){
+                str += info;
+            }
+            else{
+                str +="\n";
+                for(int i=0;i<parts;i++){
+                    str+=part[i].save();
+                }
+                for(int v=0;v<deep;v++){str+="    ";}
+            }
+            str +="</"+name+">\n";
+            return str;
         }
-        str +="</"+name+">\n";
-        return str;
     }
-}
