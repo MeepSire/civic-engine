bd06d750460baf095024115f3cf06acce991676f
philipp@dr-jean-jacques.de <philipp@dr-jean-jacques.de@d35286f8-d542-8fef-d8f9-01f4298c6c5b>
2011-03-28 05:31:58 +0000

    Sprite, Animation (and Phys2D) . Note: Phys2D library needs to be added to the project libraries.

--- civic-engine/src/core/GameCore.java
+++ civic-engine/src/core/GameCore.java
@@ -3,7 +3,10 @@
 package core;
 
 import core.events.EventHandler;
+import core.interfaces.Drawable;
 import java.awt.Point;
+import net.phys2d.math.Vector2f;
+import net.phys2d.raw.*;
 
 public class GameCore implements Runnable {
 
@@ -22,6 +25,9 @@ public class GameCore implements Runnable {
 
     private EventHandler eventHandler = new EventHandler();
 
+    // physics
+    private World world;
+
     public GameCore(){
         initialize();
         thread.start();
@@ -30,6 +36,15 @@ public class GameCore implements Runnable {
 
     public void initialize(){
         // TODO: add initialization code here
+        world = new World(new Vector2f(0, 2000), 0);
+    }
+
+    public World getWorld(){
+        return world;
+    }
+
+    public void setGravity(float x, float y){
+        world.setGravity(x, y);
     }
 
     public void setState(int state){
@@ -46,10 +61,16 @@ public class GameCore implements Runnable {
             if(state != PAUSE && state == RUN){
 
                 eventHandler.handleEvents();
-
+                world.step();   // physics
+                
                 // try rendering
                 try{
                     // TODO: rendering
+                    for(int i = 0; i < actors.length; i++){
+                        if(actors[i] instanceof Drawable == true){
+                            ((Drawable)actors[i]).draw(null);   // TODO: Draw to Volatile/Buffered Image
+                        }
+                    }
                 }
                 catch(Exception ex){
                 }
--- civic-engine/src/core/actors/TestActor.java
+++ civic-engine/src/core/actors/TestActor.java
@@ -5,52 +5,61 @@ package core.actors;
 import core.*;
 import core.events.*;
 import core.interfaces.*;
-import java.awt.Point;
+import core.sprite.Sprite;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import net.phys2d.math.Vector2f;
+import net.phys2d.raw.Body;
+import net.phys2d.raw.shapes.Box;
 
-public class TestActor extends Actor implements Moveable {
+public class TestActor extends Actor implements Drawable {
 
-    private Point position;
-    private String name;
+    private Sprite sprite;
+    private Body body;
 
-    public TestActor(Point position, String name){
-        this.position = position;
-        this.name = name;
+    private EventHandler evtHandler = new EventHandler();
+
+    public TestActor() throws IOException{
+        this.sprite = new Sprite(ImageIO.read(new File("mario.png")), new Dimension(48, 48), new Color(0, 0, 0, 0));
+        body = new Body(new Box(sprite.getFrameSize().width, sprite.getFrameSize().height), 10);
+    }
+
+    public Body getBody(){
+        return body;
     }
 
     @Override
     public void act(Key key){
 
         Key[] keys = this.getKeys();
-/*
-        if(key.getKeyCode() == keys[0].getKeyCode()){
-            EventHandler.addToEventQueue(new MoveEvent(new Point(position.x - 1, position.y), this));
-        }
-        if(key.getKeyCode() == keys[1].getKeyCode()){
-            EventHandler.addToEventQueue(new MoveEvent(new Point(position.x + 1, position.y), this));
-        }
-        if(key.getKeyCode() == keys[2].getKeyCode()){
-            EventHandler.addToEventQueue(new MoveEvent(new Point(position.x, position.y - 1), this));
-        }
-        if(key.getKeyCode() == keys[3].getKeyCode()){
-            EventHandler.addToEventQueue(new MoveEvent(new Point(position.x, position.y + 1), this));
-        }
 
-        // print to console if key pressed:
-        for(int i = 0; i < keys.length; i++){
-            if(key.getKeyCode() == keys[i].getKeyCode()){
-                EventHandler.addToEventQueue(new ConsoleEvent("Position: " + position.x + ", " + position.y));
-                //EventHandler.addToEventQueue(new SpeedTestEvent());
-            }
-        }
-*/
+        evtHandler.addToEventQueue(new PhysicsForceEvent(body, new Vector2f(0, -2000))); // force pushing body upwards
+        evtHandler.addToEventQueue(new PhysicsForceEvent(body, new Vector2f(20, 0))); // force pushing body right
+        evtHandler.addToEventQueue(new PhysicsForceEvent(body, new Vector2f(-20, 0))); // force pushing body left
+
     }
 
-    public void setPosition(Point destination) {
-        this.position = destination;
-    }
+    public void draw(Graphics2D g2d) {
 
-    public Point getPosition() {
-        return position;
+        float x = body.getPosition().getX(); 
+        float y = body.getPosition().getY();
+        
+        AffineTransform origXform = g2d.getTransform();
+        AffineTransform newXform = (AffineTransform)(origXform.clone());
+        
+        // rotate graphics around sprite
+        newXform.rotate(Math.toRadians(body.getRotation()), x, y);
+        g2d.setTransform(newXform);
+        
+        // draw sprite to the rotated graphics
+        g2d.drawImage(sprite.getActiveFrame(), (int)x, (int)y, null);
+        
+        // rotate the graphics back
+        g2d.setTransform(origXform);
+
     }
 
 }
